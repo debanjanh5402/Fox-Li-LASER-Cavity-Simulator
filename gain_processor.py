fix(physics): apply gain in proper units

gain_processor.py
@@ -3,10 +3,12 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import griddata
 
-def load_and_process_gain(filepath, X_cavity, Y_cavity, scaling_factor=1.0):
+def load_and_process_gain(filepath, X_cavity, Y_cavity, scaling_factor=100.0):
     data = pd.read_csv(filepath, sep="\t", header=0)
     data.columns = ["x", "z", "y", "gain"]
+    # Proper Scaling
     data[["x", "y"]] /= 10.0
+    data["gain"] *= scaling_factor
     x_unique = np.unique(data['x'])
     y_unique = np.unique(data['y'])
     xmean = (x_unique.max() + x_unique.min()) / 2
@@ -25,7 +27,7 @@ def load_and_process_gain(filepath, X_cavity, Y_cavity, scaling_factor=1.0):
     gain_2d = np.nan_to_num(gain_2d, nan=0.0)
     X, Y = np.meshgrid(x_unique, y_unique)
     gain_grid = griddata(np.stack([X.ravel() - xmean, Y.ravel() - ymean], axis=-1), 
-                         (gain_2d*scaling_factor).ravel(), 
+                         (gain_2d).ravel(), 
                          (X_cavity, Y_cavity), 
                          method='linear', fill_value=0)
     gain_grid = np.nan_to_num(gain_grid, nan=0.0)